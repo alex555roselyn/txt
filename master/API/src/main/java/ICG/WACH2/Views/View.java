687c4455b92d73a8155e7906824290d4422bc354
alex555roselyn <corado502@gmail.com>
2020-05-15 21:00:16 -0600

    git commit

--- master/API/src/main/java/ICG/WACH2/Views/View.java
+++
@@ -1,7 +0,0 @@
-package ICG.WACH2.views;
- 
-public class View {
-	public static interface OveralView {}
-	public static interface DetailView extends OveralView {}
-	public static interface ItemView{}
-}
